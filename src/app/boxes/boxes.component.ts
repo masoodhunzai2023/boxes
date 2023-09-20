f216f2742c338882f21df6e6b86342dba0c995df
unknown <masoodhunzai07@gmail.com>
2023-09-20 15:26:16 +0500

    bod

---
+++ src/app/boxes/boxes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-boxes',
+  templateUrl: './boxes.component.html',
+  styleUrls: ['./boxes.component.css']
+})
+export class BoxesComponent {
+  entered_value=0;
+}
